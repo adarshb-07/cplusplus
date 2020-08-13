879b64a597301897dbdbdc2111fe2908f5015661
Adarsh Bhardwaj <55309341+adarshb-07@users.noreply.github.com>
2020-08-13 22:27:24 +0530

    Add files via upload

---
+++ 3 sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout<<"Enter two number for sum ";
+	cin>>a>>b;
+
+	cout<<"Sum is "<<a+b;
+}
---
+++ 4 Find out size of data type.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"size of int: "<<sizeof(int);
+    cout<<"\nsize of char: "<<sizeof(char);
+    cout<<"\nsize of float: "<<sizeof(float);
+    cout<<"\nsize of long: "<<sizeof(long);
+    cout<<"\nsize of double: "<<sizeof(double);
+    cout<<"\nsize of bool: "<<sizeof(bool);
+
+    return 0;
+}
